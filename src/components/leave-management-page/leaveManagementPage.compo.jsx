9b27c1372015ad7070355145c455b9f22ddd9138
Abouzar <abouzar.azarpira@gmail.com>
2021-02-27 00:24:10 +0800

    add tabs for supervisor page

--- src/components/leave-management-page/leaveManagementPage.compo.jsx
+++ src/components/leave-management-page/leaveManagementPage.compo.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Tab,TabList, TabPanel, Tabs } from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
+import AddInterpreterToDB from "../add-interpreter-todb/addInterpreterToDB.compo";
 import LeaveManangementStage from "../leaveManagementStage.compo";
 
 
 
 const LeaveManagementPage =()=>{
-    return <LeaveManangementStage/>
+    return (
+        <Tabs>
+            <TabList>
+                <Tab>Leave Management</Tab>
+                <Tab >Interpreter Management</Tab>
+            </TabList>
+            <TabPanel>
+                <LeaveManangementStage/>
+            </TabPanel>
+            <TabPanel>
+                <AddInterpreterToDB/>
+            </TabPanel>
+        </Tabs>
+        
+    )
 }
 
 
